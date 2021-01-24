dcc865d3adf833ad110fcad02afbe641e800407f
Book <66485809+VoltJimathy@users.noreply.github.com>
2021-01-24 11:11:21 +0000

    .

--- lib/cogs/Misc.py
+++ lib/cogs/Misc.py
@@ -1,8 +1,12 @@
 from discord.ext.commands import Cog
 from discord.ext.commands import command, has_permissions, CheckFailure
 from discord import Embed
-from datetime import datetime
+from discord import __version__ as discord_version
+from datetime import datetime, timedelta
 from ..db import db # type: ignore
+from psutil import Process, virtual_memory
+from platform import python_version
+from time import time
 
 class Misc(Cog):
 
@@ -33,6 +37,35 @@ class Misc(Cog):
                 colour=0xFF0000,
                 timesptamp=datetime.utcnow())
             await ctx.send(embed=embed)
+    
+    @command(name="info", aliases=["stats"])
+    async def info(self, ctx):
+        embed = Embed(
+            title="Bot stats",
+            colour=ctx.author.colour,
+            thumbnail=self.bot.user.avatar_url,
+            timestamp=datetime.utcnow())
+        
+        proc = Process
+        with proc.oneshot(self=self):
+            uptime = timedelta(seconds=time()-proc.create_time())
+            cpu_time = timedelta(seconds=(cpu := proc.cpu_times()).system + cpu.user)
+            mem_total = virtual_memory().total / (1024**2)
+            mem_of_total = proc.memory_percent()
+            mem_usage = mem_total / (mem_of_total*100)
+        
+        fields = [("Bot version", self.bot.VERSION, True),
+                  ("Python version", python_version(), True),
+                  ("Discord.py version", discord_version(), True),
+                  ("Uptime", uptime, True),
+                  ("CPU time", cpu_time, True),
+                  ("Memory usage", f"{mem_usage:,.3f} / {mem_total:,.0f} ({mem_of_total}%)", True),
+                  ("Users", f"{self.bot.guild.member_count:,}", True)]
+
+        for name, value, inline in fields:
+            embed.add_field(name=name, value=value, inline=inline)
+        
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Misc(bot))
